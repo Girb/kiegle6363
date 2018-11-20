30fc6184e0ee25abd622410702a453d6ab8377ec
Eivind Sommersten <eivind@machina.no>
2018-11-20 21:55:03 +0100

    polish

--- web/dotcom/generator/index.js
+++ web/dotcom/generator/index.js
@@ -3,13 +3,14 @@ const config = require('./config.json');
 const Page = require('./page');
 
 const id = 1;
-axios.get(`http://${config.server.address}:${config.server.port}/api/competitions/${id}`).then((response) => {
-    const competition = response.data;
-    axios.get(`http://${config.server.address}:${config.server.port}/api/competitions/${id}/rounds`).then((rresp) => {
-        const rounds = rresp.data;
-        new Page(competition, rounds).render();
+function go() {
+    axios.get(`http://${config.server.address}:${config.server.port}/api/competitions/${id}`).then((response) => {
+        const competition = response.data;
+        axios.get(`http://${config.server.address}:${config.server.port}/api/competitions/${id}/rounds`).then((rresp) => {
+            const rounds = rresp.data;
+            new Page(competition, rounds).render();
+        });
     });
-});
+}
 
-
-console.log(config.server.address);
+setInterval(go, 5000);
